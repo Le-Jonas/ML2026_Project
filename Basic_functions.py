da5970ed65e1e1fc418a8aa1dacb3b87f0193233
Jonas <wrt770@alumni.ku.dk>
2026-05-18 15:44:04 +0200

    Finsihed first part of basic_functions

--- Basic_functions.py
+++ Basic_functions.py
@@ -2,11 +2,22 @@ import numpy as np
 import os
 from numba import njit
 
+def int_convert_true(x):
+    try:
+        int(x)
+        return 1
+    except ValueError:
+        return 0
 
-def convert_to_data(file):
+def date_to_float(date_str):
+    if int_convert_true(date_str):
+        return float(date_str)
+    year, month, day = date_str.split("-")
+    return float(year) + float(month) / 12 + float(day) / 365
+
+def convert_to_data(raw):
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "æ", "ø", "å", " "]
     data = [0] * (len(alphabet) + 1)
-    raw = open(file, "r", encoding="utf-8").read().strip()
     N = len(raw)
 
     total_num = 0
@@ -20,7 +31,21 @@ def convert_to_data(file):
 
 def read_files(path):
     data = []
+    label = []
     for file in os.listdir(path):
-        data.append(convert_to_data(os.path.join(path, file)))
-    return data
+        raw = open(os.path.join(path, file), "r", encoding="utf-8").read()
+        idxs = np.array([i for i, ch in enumerate(raw) if ch == "\n"])
+        diff = np.diff(idxs)
+        try:
+            idx = np.where((diff[:-1] == 1) & (diff[1:] == 1))[0][0]
+        except IndexError:
+            print(np.where((diff[:-1] == 1) & (diff[1:] == 1)))
+            raise IndexError("File {} does not have the expected format.".format(file))
+        text = raw[idxs[idx]+3:]
+        header = raw[:idxs[idx]]
+        header_lines = header.splitlines()
+        date = header_lines[1].split(": ")[1]
+        data.append(convert_to_data(text.strip()))
+        label.append(date_to_float(date))
+    return data, label
 
